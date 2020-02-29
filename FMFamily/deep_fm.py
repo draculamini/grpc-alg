28eabc6e7a4a7b7c343e5113fc857c9b972dc7a2
wuxikun <xikun.wu@ushow.media>
2020-02-29 12:29:55 +0800

    deepFM

--- FMFamily/deep_fm.py
+++ FMFamily/deep_fm.py
@@ -3,143 +3,128 @@
 # @author: wuxikun
 # @date: '2020/2/25'
 
-
 import numpy as np
 import tensorflow as tf
 import sys
 from FMFamily.build_data import load_data
 
-
-class HyperArg():
+class HyperArgs():
 
     # 模型参数
     feature_size = 100
-    field_size = 15
     embedding_size = 256
     deep_layers = [512, 256, 128]
+    field_size = 15
 
-
-    # 训练参数
     epoch = 3
     batch_size = 64
-    lr_rate = 1.0
+    learning_rate = 0.05
     l2_reg_rate = 0.01
-    is_training = True
+    is_training = False
     checkpoint_dir = '/Users/wuxikun/Documents/gRPC/python3/FMFamily/model/'
 
 
 class DeepFM():
 
-    def __init__(self, arg):
+    def __init__(self, args):
 
-        self.embedding_size = arg.embedding_size
-        self.deep_layers = [512, 256, 128]
-        self.field_size = arg.field_size
-        self.feature_size = arg.feature_size
-        self.weight = dict()
-        self.deep_activation = tf.nn.relu
-        self.learning_rate = arg.lr_rate
+        self.embedding_size = args.embedding_size
+        self.deep_layers = args.deep_layers
         self.l2_reg_rate = args.l2_reg_rate
+        self.field_size = args.field_size
+        self.feature_size = args.feature_size
+        self.learning_rate = args.learning_rate
+        self.deep_activation = tf.nn.relu
 
-        self.epoch = args.epoch
+        self.weight = dict()
         self.build_model()
-
 
     def build_model(self):
 
         # 输入数据
-        self.feat_index = tf.placeholder(tf.int32, [None, None], "feature_index")
-        self.feat_value = tf.placeholder(tf.float32, [None, None], "feature_value")
+        self.feat_index = tf.placeholder(tf.int32, [None, None], "feat_indx")
+        self.feat_value = tf.placeholder(tf.float32, [None, None], "feat_value")
         self.label = tf.placeholder(tf.float32, [None, None], "label")
 
-        # 1.embeding层参数 FM公式中的V
+        # 第一个因子
+        self.weight["first_actor_weight"] = tf.Variable(
+            tf.random_normal([self.feature_size, 1], 0.0, 0.01), name="first_actor_weight"
+        )
 
+        self.fm_first_actor = tf.multiply(tf.nn.embedding_lookup(
+            self.weight["first_actor_weight"],  self.feat_index
+        ), tf.reshape(self.feat_value, [-1, self.field_size, 1]))
+        self.fm_first_actor = tf.reduce_sum(self.fm_first_actor, 2)
+
+        # 第二个因子
         self.weight["embedding_weight"] = tf.Variable(
-            tf.random_normal([self.feature_size, self.embedding_size], 0.0, 0.01),
-            name="embedding_weight"
+            tf.random_normal([self.feature_size, self.embedding_size],  0.0, 0.01), name="embedding_weight"
         )
+        self.embed_part_weight = tf.nn.embedding_lookup(self.weight["embedding_weight"], self.feat_index)
 
-        # 2.1 FM 一次项(∑WX) 的参数
-        self.weight["fm_first_weight"] = tf.Variable(
-            tf.random_normal([self.feature_size, 1], 0.0, 0.01),
-            name="fm_first_weight"
-        )
+        # embed_part_weight -> (batch_size, field_size, embedding_size)
+        self.embed_part = tf.multiply(self.embed_part_weight, tf.reshape(self.feat_value, [-1, self.field_size, 1]))
 
-        # 2.2 FM 二次项参数
+        self.second_factor_sum = tf.reduce_sum(self.embed_part, 1)
+        self.second_factor_sum_square = tf.square(self.second_factor_sum)
 
-        # 3. deep层各层参数
-        cnt_of_deep_layers = len(self.deep_layers)
-        deep_input_size = self.field_size * self.embedding_size
+        self.second_factor_square = tf.square(self.embed_part)
+        self.second_factor_square_sum = tf.reduce_sum(self.second_factor_square, 1)
+
+        self.second_factor = 0.5 * tf.subtract(self.second_factor_sum_square, self.second_factor_square_sum)
+
+        # deep part
+        deep_input_size = self.embedding_size * self.field_size
         init_value = np.sqrt(2.0 / (deep_input_size + self.deep_layers[0]))
 
         self.weight["layer_0"] = tf.Variable(
-            tf.random_normal([deep_input_size, self.deep_layers[0]], 0.0, init_value), dtype=tf.float32, name="layer_0"
+            tf.random_normal([self.embedding_size * self.field_size, self.deep_layers[0]], 0.0, init_value), name="layer_0"
+        )
+        self.weight["bias_0"] = tf.Variable(
+            tf.random_normal([1, self.deep_layers[0]], 0.0, init_value), name="bias_0"
         )
 
-        self.weight["bias_0"] = tf.Variable(
-            tf.random_normal([1, self.deep_layers[0]], 0.0, init_value), dtype=tf.float32, name="bias_0"
-        )
-        # 其它层用的参数
-        if cnt_of_deep_layers > 1:
-            for i in range(1, cnt_of_deep_layers):
-                init_method = np.sqrt(2.0 / (self.deep_layers[i - 1] + self.deep_layers[i]))
+        cnt_hidden_layer = len(self.deep_layers)
+
+        if cnt_hidden_layer > 1:
+            for i in range(1, cnt_hidden_layer):
+                init_value = np.sqrt(2.0 / (self.deep_layers[i - 1] + self.deep_layers[i]))
+
                 self.weight["layer_" + str(i)] = tf.Variable(
-                    tf.random_normal([self.deep_layers[i-1], self.deep_layers[i]], 0.0, init_method), dtype=tf.float32,
-                    name="layer_" + str(i)
+                    tf.random_normal([self.deep_layers[i-1], self.deep_layers[i]], 0.0, init_value), name="layer_" + str(i)
                 )
                 self.weight["bias_" + str(i)] = tf.Variable(
-                    tf.random_normal([1, self.deep_layers[i]], 0.0, init_value), dtype=tf.float32, name="bias_" + str(i)
+                    tf.random_normal([1, self.deep_layers[i]], 0.0, init_value), name="bias_" + str(i)
                 )
 
-        # 最后一层 合并
-        merge_layer_size = self.deep_layers[-1] + self.field_size + self.embedding_size
+        self.deep_embedding = tf.reshape(self.embed_part, [-1, deep_input_size])
+        for i in range(0, cnt_hidden_layer):
+            self.deep_embedding = tf.add(
+                tf.matmul(self.deep_embedding, self.weight["layer_" + str(i)]),
+                self.weight["bias_" + str(i)]
+            )
+            self.deep_embedding = self.deep_activation(self.deep_embedding)
 
-        init_method = np.sqrt(np.sqrt(2.0 / (merge_layer_size + 1)))
+        merge_layer_size = self.field_size + self.embedding_size + self.deep_layers[-1]
+        init_value = np.sqrt(np.sqrt(2.0 / (merge_layer_size + 1)))
 
         self.weight["merge_layer"] = tf.Variable(
-            tf.random_normal([merge_layer_size, 1], 0, init_method), name="merge_layer"
+            tf.random_normal([merge_layer_size, 1], 0, init_value), name="merge_layer"
         )
+
         self.weight["merge_bias"] = tf.Variable(
             tf.constant(0.01), dtype=tf.float32, name="merge_bias"
         )
 
-        # 嵌入层
-        self.embedding_index = tf.nn.embedding_lookup(self.weight["embedding_weight"], self.feat_index)
+        self.fm_part = tf.concat([self.fm_first_actor, self.second_factor], axis=1)
 
-        self.embedding_part = tf.multiply(self.embedding_index, tf.reshape(
-            self.feat_value, [-1, self.field_size, 1]
-        ))
-
-        # self.weight['fm_first_weight'] batch * feature_size * 1
-        self.fm_first_factor = tf.nn.embedding_lookup(self.weight['fm_first_weight'], self.feat_index)
-
-        self.fm_first_factor = tf.multiply(self.fm_first_factor, tf.reshape(self.feat_value, [-1, self.field_size, 1]))
-
-        self.fm_first_factor = tf.reduce_sum(self.fm_first_factor, 2)  # (batch_size, field_size, 1)
-
-        self.sum_second_factor = tf.reduce_sum(self.embedding_part, 1)
-        self.sum_second_factor_square = tf.square(self.sum_second_factor)
-
-        self.square_second_factor = tf.square(self.embedding_part)
-        self.square_second_factor_sum = tf.reduce_sum(self.square_second_factor, 1)
-
-        self.fm_second_factor = 0.5 * (tf.subtract(self.sum_second_factor_square, self.square_second_factor_sum))
-
-        self.fm_part = tf.concat([self.fm_first_factor, self.fm_second_factor], axis=1)
-
-        self.deep_embedding = tf.reshape(self.embedding_part, [-1, self.field_size * self.embedding_size])
-
-        for i in range(0, len(self.deep_layers)):
-            self.deep_embedding = self.deep_activation(tf.add(tf.matmul(self.deep_embedding, self.weight["layer_" + str(i)]),
-                                         self.weight["bias_" + str(i)]))
-
-        concat_all = tf.concat([self.fm_part, self.deep_embedding], axis=1)  # field_size, embedsize, deep_layer[-1]
-        self.out = tf.add(tf.matmul(concat_all, self.weight["merge_layer"]), self.weight["merge_bias"])
+        self.out = tf.add(tf.matmul(tf.concat([self.fm_part, self.deep_embedding], axis=1),
+                                    self.weight["merge_layer"]), self.weight["merge_bias"])
 
         self.out = tf.nn.sigmoid(self.out)
 
         self.loss = -tf.reduce_mean(
-            self.label * tf.log(self.out + 1e-24) + (1-self.label) * tf.log(1 - self.out + 1e-24))
+            self.label * tf.log(self.out + 1e-24) + (1 - self.label) * tf.log(1 - self.out + 1e-24))
 
         self.loss += tf.contrib.layers.l2_regularizer(self.l2_reg_rate)(self.weight["merge_layer"])
 
@@ -179,7 +164,6 @@ class DeepFM():
         saver = tf.train.Saver()
         saver.restore(sess, save_path=path)
 
-
 def get_batch(Xi, Xv, y, batch_size, index):
     start = index * batch_size
     end = (index + 1) * batch_size
@@ -188,7 +172,7 @@ def get_batch(Xi, Xv, y, batch_size, index):
 
 
 if __name__ == '__main__':
-    args = HyperArg()
+    args = HyperArgs()
 
     gpu_config = tf.ConfigProto()
     gpu_config.gpu_options.allow_growth = True
@@ -221,6 +205,8 @@ if __name__ == '__main__':
                 X_index, X_value, y = get_batch(data['xi'], data['xv'], data['y_train'], args.batch_size, j)
                 result = Model.predict(sess, X_index, X_value)
                 print(result)
+
+
 
 
 
